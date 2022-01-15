df427b61aad342fbde28a9bac61951eca42e13f9
farhah din <farhahdin97@hotmail.com>
2022-01-15 16:51:42 +0000

    variables set to create the timer

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Selecting Elements
+/* start button*/
 var startButton = document.querySelector(".startButton");
 var displayBox = document.querySelector(".displayBox");
+
+/*timer*/
+var timerCount = quizBox.querySelector(".timerSeconds");
+var gameSetting = {
+    score: 0,
+    timeTotal: 60
+}
 
 // Start Quiz Button Click Functionality
 // displays "Display Box"
@@ -8,3 +16,5 @@ var displayBox = document.querySelector(".displayBox");
 startButton.onclick = function () {
     displayBox.classList.add("activeDisplayBox");
 }
+
+//Timer 
