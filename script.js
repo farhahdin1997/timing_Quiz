f8de30abe7d5dccf4448bf2bccd8627b68483b41
farhah din <farhahdin97@hotmail.com>
2022-01-15 17:10:49 +0000

    timer added

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var gameSetting = {
     score: 0,
     timeTotal: 60
 }
+var result = document.querySelector(".scoreResults");
+var quizBox = document.querySelector(".quizBox");
+var finishText = document.querySelector(".finishText");
+var userScore = document.querySelector(".answersCorrect");
 
 // Start Quiz Button Click Functionality
 // displays "Display Box"
@@ -19,8 +23,37 @@ startButton.onclick = function () {
 
 // create functionality to timer
 function startTimer(resetTimer) {
-    
+    counter = setInterval(timer ,1000) 
+    // SetInterval method repeats a given function at every given time-interval. 
+    //Timer is the function that will exucuted and 1000 indicates the length oif time of the time-interval between each execution.
+    function timer() {
+        if (resetTimer){
+            clearInterval(counter)
+        };
+        timerCount.timerCount = gameSetting.timeTotal;
+        gameSetting.timeTotal --;
+        if (gameSetting.timeTotal<9){
+            var singleNo = timerCount.textContent;
+            timerCount.textContent = "0" + singleNo;
+        }
+        if(gameSetting.timeTotal <0 ){
+            clearInterval(counter);
+            timerCount.textContent ="00";
+        }
+    }
+} 
 
-
-
-}
+let defaultInterval = () => setInterval(() => {
+    const activeResultsPage = document.querySelector('.activeResults')
+    const activeHighScorePage = document.querySelector('.activeHighScore')
+    if (activeResultsPage || activeHighScorePage) {
+        clearInterval(defaultInterval);
+    } else if (timerCount.innerHTML === "00") {
+        result.classList.add('activeResults');
+        quizBox.classList.remove('activeQuizBox');
+        userScore.innerHTML = gameSetting.score;
+        finishText.innerHTML = 'You ran out of time!'
+        clearInterval(defaultInterval);
+    }
+}, 1000);
+defaultInterval()
